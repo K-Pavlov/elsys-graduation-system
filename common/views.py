5c620b156a106e7f950ce8c470a8f01cc9616b36
k-pavlov <kaloian.pavlov@abv.bg>
2015-03-09 20:39:56 +0200

    404 and 500 error pages

--- common/views.py
+++ common/views.py
@@ -140,3 +140,19 @@ def abstr_delete(request, id, model):
         return HttpResponse(json.dumps('Success'), content_type = "application/json")
 
     raise Http404
+
+from django.shortcuts import render_to_response
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
