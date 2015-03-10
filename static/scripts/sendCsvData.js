da180ea2db53a3dbb1b0a5e7d60f1e9b2eedd94e
unknown <kaloian.pavlov@gmail.com>
2015-03-10 02:03:59 +0200

    Static files moved to root directory

---
+++ static/scripts/sendCsvData.js
@@ -0,0 +1,85 @@
+﻿/// <reference path="../libs/jquery-1.10.2.js" />
+$(document).ready(function () {
+    var $button = $('#send-button');
+    $button.click(function () {
+        markData();
+        sendData();
+    });
+
+    //var $selects = $('.choosen-data-type');
+    //$selects.each(function () {
+    //    var $this = $(this),
+    //        previous;
+
+    //    $this.focus(function () {
+    //        previous = $(this).children(':selected');
+    //    }).change(function () {
+    //        var selectValue = $this.val(),
+    //            $selectsToRemove;
+         
+    //        $selectsToRemove = $($.grep($('.choosen-data-type'), function (item) {
+    //            var $item = $(item);
+    //            if ($item.val() === selectValue) {
+    //                return false;
+    //            }
+
+    //            return true;
+    //        }));
+
+    //        $selectsToRemove.each(function () {
+    //            var $self = $(this),
+    //                $itemToDetach;
+
+    //            $self.children().each(function () {
+    //                var $this = $(this);
+    //                if ($this.val() === selectValue) {
+    //                    $this.detach();
+    //                    return;
+    //                }
+    //            });
+    //        });
+
+    //        previous = $this.children(':selected');
+    //    });
+    //});
+
+    function markData() {
+        var $selects = $('.choosen-data-type');
+        $selects.each(function () {
+            var $this = $(this);
+            var $dataForType = $('.' + $this.attr('id').replace('select-', ''));
+            $dataForType.each(function () {
+                var $self = $(this);
+                $self.attr('type', $this.val());
+            });
+        });
+    }
+
+    function sendData() {
+        var $dataRows = $('.row-data'),
+            dataObjects = [];
+
+        $dataRows.each(function () {
+            var $this = $(this),
+                $data = $this.children('td'),
+                cleanData = {};
+
+            $data.each(function () {
+                var $self = $(this),
+                    type = $self.attr('type');
+                if (type) {
+                    cleanData[type] = $self.html().trim();
+                }
+            });
+            dataObjects.push(cleanData);
+        });
+
+        var json = JSON.stringify(dataObjects);
+        $.post('/' + $('#send-button').attr('view-name') + '/upload/', json).then(function (response) {
+            toastr.success('Успешно създадохте записите.');
+            window.location.href = JSON.parse(response).redir;
+        }, function (err) {
+            toastr.error('Моля, опитайте отново.');
+        });
+    }
+});
---
+++ static/scripts/table.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    function addPageActions() {
+        var $prevPage = $('#previous-page'),
+            $nextPage = $('#next-page'),
+            $firstPage = $('#first-page'),
+            $lastPage = $('#last-page'),
+            $pageLinks = $('.page-link');
+
+        addClick($firstPage);
+        addClick($prevPage);
+        $pageLinks.each(function () {
+            var $this = $(this);
+            addClick($this);
+        });
+
+        addClick($nextPage);
+        addClick($lastPage);
+    }
+
+    function addClick($target) {
+        var $replaceDiv = $('#table-replace'),
+            viewName = $('#create-link').attr('href').split('/')[1];
+
+        $target.click(function () {
+            $.get('/' + viewName + '/page/' + $target.attr('page-number') + '/')
+                .then(function (data) {
+                    $replaceDiv.html(data);
+                    makeSortable();
+                    addPageActions();
+                    addDeletation();
+                });
+        });
+    }
+
+    function makeSortable() {
+        var th = $('#all-table th').not('.ignore'),
+        inverse = false;
+
+        th.click(function () {
+            var header = $(this),
+                index = header.index();
+
+            $('#all-table').find('td')
+                .filter(function () {
+                    return $(this).index() === index;
+                })
+                .sort(function (a, b) {
+                    a = $(a).text();
+                    b = $(b).text();
+                    return (isNaN(a) || isNaN(b) ? a > b : +a > +b) ? (inverse ? -1 : 1) : (inverse ? 1 : -1);
+                }, function () {
+                    return this.parentNode;
+                });
+
+            inverse = !inverse;
+        });
+    }
+
+    function addDeletation() {
+        var $form = $('.delete-form');
+        $form.each(function () {
+            var $this = $(this),
+                $objectToDelete = $('#' + $this.attr('object-id'));
+
+            ajaxOvverideDelForm($this, $objectToDelete);
+        });
+    }
+
+    addPageActions();
+    makeSortable();
+    addDeletation();
+});
